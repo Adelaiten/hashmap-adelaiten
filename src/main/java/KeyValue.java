25cf931a41ccf8de94b1ace709b280bc5cd72fc9
Adelaiten <karoltrzaska19@gmail.com>
2019-02-14 17:11:20 +0100

    key value container

---
+++ src/main/java/KeyValue.java
@@ -0,0 +1,20 @@
+public class KeyValue<T, V> {
+    private T key;
+    private V value;
+
+    public T getKey() {
+        return key;
+    }
+
+    public void setKey(T key) {
+        this.key = key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+}
